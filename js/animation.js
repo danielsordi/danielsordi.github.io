dc69e14cfab318412baed928c1e2f21eeb58c036
danielsordi <daniel.b.sordi@gmail.com>
2023-07-01 09:00:20 +1200

    Additional updates on the resume and home

--- js/animation.js
+++ js/animation.js
@@ -1,13 +1,13 @@
 window.addEventListener("load", function () {
 
     // Get all elements inside the nav bar
-    const navItems = document.querySelectorAll('nav .nav-link, navbar-brand');
+    const navItems = document.querySelectorAll('nav .nav-link');
 
     // Add the desired class to each element
     navItems.forEach(item => {
-        item.classList.add('text-light-emphasis');
-    });
-    
+        item.classList.add('black-text');
+    }); 
+
 
 });
 
